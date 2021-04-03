7283f518732c3634291984c24334b9d4e8d97436
Rory Gudka <rory.gudka@gmail.com>
2021-04-03 15:46:18 -0400

    Implemented an answer feature that does not allow a user to select another and offers visual feedback

--- src/App.js
+++ src/App.js
@@ -6,27 +6,50 @@ import "./App.css";
 
 export default function App() {
   const [questions, setQuestions] = useState([]);
+
+
+
+  const handleAnswerClick = (e, index, answer) => {
+    let question = questions[index];
+    if(!question.answered) {
+      let newQuestions = questions;
+      newQuestions[index].answered = true;
+      newQuestions[index].isCorrect = question.correct_answer == answer;
+      newQuestions[index].answerClicked = answer;
+      setQuestions([...newQuestions]);
+    }
+  }
+
+
+
+
+
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
       .then((res) => res.json())
       .then((res) => {
+        for(let i = 0; i < res.results.length; i++) {
+          res.results[i].answered = false;
+          let answers = [res.results[i].correct_answer, ...res.results[i].incorrect_answers];
+          for(let j = 0; j < answers.length; j++) {
+            const rand = Math.floor(Math.random() * answers.length);
+            const temp = answers[j];
+            answers[j] = answers[rand];
+            answers[rand] = temp;
+          }
+          res.results[i].answers = answers;
+        }
+        
         setQuestions(res.results);
       });
   }, []);
 
   let answerList = [];
   for(let i = 0; i < questions.length; i++) {
-    let answers = [questions[i].correct_answer, ...questions[i].incorrect_answers];
-    console.log(answers[0]);
-    for(let i = 0; i < answers.length; i++) {
-      const rand = Math.floor(Math.random() * answers.length);
-      const temp = answers[i];
-      answers[i] = answers[rand];
-      answers[rand] = temp;
-    }
+    let answers = questions[i].answers;
     answerList.push(answers.map(answer => (
     <div className="answerWrapper">
-      <Button key={nanoid()} variant="outlined" color="primary">{answer}</Button>
+      <Button key={nanoid()} variant="outlined" color="primary" className={questions[i].answered && answer == questions[i].correct_answer ? "correctAnswer" : questions[i].answered && questions[i].answerClicked == answer ? "incorrectAnswer" : "none"} onClick={(e) => handleAnswerClick(e, i, answer)}>{answer}</Button>
     </div>
     )));
   }
