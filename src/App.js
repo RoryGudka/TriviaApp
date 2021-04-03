f96e435f26d8713656647b0ba7c511b877d709dd
Rory Gudka <rory.gudka@gmail.com>
2021-04-03 14:26:08 -0400

    Initialized the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+export default function App() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setQuestions(res.results);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hey, Launch! 👋</h1>
+      {questions.map((question) => (
+        <h1>{question.question}</h1>
+      ))}
     </div>
   );
 }
-
-export default App;
