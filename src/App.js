25a0eceb029ea70ccc8c16fc82fbdfd3d4e61db0
Rory Gudka <rory.gudka@gmail.com>
2021-04-06 18:53:40 -0400

    Added a feature that allows the user to change the category and amount

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 import { nanoid } from 'nanoid'
 import "./App.css";
 
 export default function App() {
   const [questions, setQuestions] = useState([]);
+  const [subject, setSubject] = useState(9);
+  const [numQuestions, setNumQuestions] = useState(10);
   
   
-  
-  const  decodeHTML = html => {
-    var txt = document.createElement('textarea');
-    txt.innerHTML = html;
-    return txt.value;
-  };
-  const handleAnswerClick = (e, index, answer) => {
-    let question = questions[index];
-    if(!question.answered) {
-      let newQuestions = questions;
-      newQuestions[index].answered = true;
-      newQuestions[index].isCorrect = question.correct_answer == answer;
-      newQuestions[index].answerClicked = answer;
-      setQuestions([...newQuestions]);
+
+  const handleChange = val => {
+    setSubject(val.target.value);
+  }
+  const handleNumChange = val => {
+    setNumQuestions(val.target.value);
+  }
+  const handleSearch = e => {
+    let num = parseInt(numQuestions);
+    if(typeof num == "number" ) {
+      if(num < 1) num = 1;
+      else if(num > 100) num = 100;
+      const url = `https://opentdb.com/api.php?amount=${num}&category=${subject}`;
+      search(url);
     }
   }
-
-
-
-
-
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10")
+  const search = url => {
+    fetch(url)
       .then((res) => res.json())
       .then((res) => {
         for(let i = 0; i < res.results.length; i++) {
@@ -50,17 +52,33 @@ export default function App() {
           }
           res.results[i].answers = answers;
         }
-        
         setQuestions(res.results);
       });
-  }, []);
+  }
+  const  decodeHTML = html => {
+    var txt = document.createElement('textarea');
+    txt.innerHTML = html;
+    return txt.value;
+  };
+  const handleAnswerClick = (e, index, answer) => {
+    let question = questions[index];
+    if(!question.answered) {
+      let newQuestions = questions;
+      newQuestions[index].answered = true;
+      newQuestions[index].isCorrect = question.correct_answer === answer;
+      newQuestions[index].answerClicked = answer;
+      setQuestions([...newQuestions]);
+    }
+  }
+
+
 
   let answerList = [];
   for(let i = 0; i < questions.length; i++) {
     let answers = questions[i].answers;
     answerList.push(answers.map(answer => (
-    <div className="answerWrapper">
-      <Button key={nanoid()} variant="outlined" color="primary" className={questions[i].answered && answer == questions[i].correct_answer ? "correctAnswer" : questions[i].answered && questions[i].answerClicked == answer ? "incorrectAnswer" : "none"} onClick={(e) => handleAnswerClick(e, i, answer)}>{answer}</Button>
+    <div key={nanoid()} className="answerWrapper">
+      <Button variant="outlined" color="primary" className={questions[i].answered && answer === questions[i].correct_answer ? "correctAnswer" : questions[i].answered && questions[i].answerClicked === answer ? "incorrectAnswer" : "none"} onClick={(e) => handleAnswerClick(e, i, answer)}>{answer}</Button>
     </div>
     )));
   }
@@ -76,9 +94,45 @@ export default function App() {
       </Paper>
     )
   })
+
+
+
   return (
     <div>
-      {questionList}
+      <div id="searchContainer">
+        <FormControl>
+          <InputLabel>Category</InputLabel>
+          <Select id="demo-simple-select" value={subject} onChange={handleChange}>
+            <MenuItem value={9}>General Knowledge</MenuItem>
+            <MenuItem value={10}>Entertainment: Books</MenuItem>
+            <MenuItem value={11}>Entertainment: Film</MenuItem>
+            <MenuItem value={12}>Entertainment: Music</MenuItem>
+            <MenuItem value={13}>Entertainment: Musicals & Theaters</MenuItem>
+            <MenuItem value={14}>Entertainment: Television</MenuItem>
+            <MenuItem value={15}>Entertainment: Video Games</MenuItem>
+            <MenuItem value={16}>Entertainment: Board Games</MenuItem>
+            <MenuItem value={17}>Science & Nature</MenuItem>
+            <MenuItem value={18}>Science: Computers</MenuItem>
+            <MenuItem value={19}>Science: Mathematics</MenuItem>
+            <MenuItem value={20}>Mythology</MenuItem>
+            <MenuItem value={21}>Sports</MenuItem>
+            <MenuItem value={22}>Geography</MenuItem>
+            <MenuItem value={23}>History</MenuItem>
+            <MenuItem value={24}>Politics</MenuItem>
+            <MenuItem value={25}>Art</MenuItem>
+            <MenuItem value={26}>Celebrities</MenuItem>
+            <MenuItem value={27}>Animals</MenuItem>
+            <MenuItem value={28}>Vehicles</MenuItem>
+            <MenuItem value={29}>Entertainment: Comics</MenuItem>
+            <MenuItem value={30}>Science: Gadgets</MenuItem>
+            <MenuItem value={31}>Entertainment: Japanese Anime & Manga</MenuItem>
+            <MenuItem value={32}>Entertainment: Catoons and Animation</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField label="Number of Questions" className="specific" value={numQuestions} onChange={handleNumChange}></TextField>
+        <Button id="searchBtn" variant="contained" color="primary" onClick={handleSearch}>Search</Button>
+      </div>
+      {questionList.length !== 0 ? questionList : <p id="noQuestions">Make a search to get questions</p>}
     </div>
   );
 }
